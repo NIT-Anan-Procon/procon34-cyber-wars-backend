90a293c496527ffea50a4eddbab3b37d25798187
ORIKAMIxNEP <ta.orikami@gmail.com>
2023-05-14 21:13:01 +0000

    first commit

---
+++ src/main/java/com/example/procon34_CYBER_WARS_backend/Procon34CyberWarsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.procon34_CYBER_WARS_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Procon34CyberWarsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Procon34CyberWarsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/procon34_CYBER_WARS_backend/Procon34CyberWarsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.procon34_CYBER_WARS_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Procon34CyberWarsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
