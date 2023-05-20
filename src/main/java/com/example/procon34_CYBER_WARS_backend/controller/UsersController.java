c57742f163620b0d07e7d404b0ed21d304b7f259
ORIKAMIxNEP <ta.orikami@gmail.com>
2023-05-21 07:52:00 +0900

    RequestBodyの変更

--- src/main/java/com/example/procon34_CYBER_WARS_backend/controller/UsersController.java
+++ src/main/java/com/example/procon34_CYBER_WARS_backend/controller/UsersController.java
@@ -1,22 +1,23 @@
 package com.example.procon34_CYBER_WARS_backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.procon34_CYBER_WARS_backend.dto.UsersRegisterRequest;
 import com.example.procon34_CYBER_WARS_backend.service.UsersService;
 
 @RestController
+@RequestMapping("/user")
 public class UsersController {
 
     @Autowired
     UsersService usersService;
 
-    @RequestMapping(value = "/register-user", method = RequestMethod.POST)
-    public void registerUser(@ModelAttribute UsersRegisterRequest usersRegisterRequest) {
+    @PostMapping("/register-user")
+    public void registerUser(@RequestBody UsersRegisterRequest usersRegisterRequest) {
         usersService.registerUser(usersRegisterRequest);
     }
 
