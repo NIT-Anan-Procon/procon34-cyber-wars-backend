292f85d916203b836490a81fa3e87dcc850a43a1
ORIKAMIxNEP <ta.orikami@gmail.com>
2023-05-21 07:56:32 +0900

    test

--- src/main/java/com/example/procon34_CYBER_WARS_backend/dto/UsersRegisterRequest.java
+++ src/main/java/com/example/procon34_CYBER_WARS_backend/dto/UsersRegisterRequest.java
@@ -8,22 +8,7 @@ import lombok.Data;
 public class UsersRegisterRequest implements Serializable {
 
     private String name;
+
     private String password;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
 }
