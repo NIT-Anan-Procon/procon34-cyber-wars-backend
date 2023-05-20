0d68b9bcc8e79c4260ffcce6d47d038b0c4979b5
ORIKAMIxNEP <ta.orikami@gmail.com>
2023-05-21 08:49:42 +0900

    validationによる制約を追加

--- src/main/java/com/example/procon34_CYBER_WARS_backend/controller/UsersController.java
+++ src/main/java/com/example/procon34_CYBER_WARS_backend/controller/UsersController.java
@@ -1,6 +1,8 @@
 package com.example.procon34_CYBER_WARS_backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.procon34_CYBER_WARS_backend.dto.UsersRegisterRequest;
 import com.example.procon34_CYBER_WARS_backend.service.UsersService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
@@ -17,8 +21,14 @@ public class UsersController {
     UsersService usersService;
 
     @PostMapping("/register-user")
-    public void registerUser(@RequestBody UsersRegisterRequest usersRegisterRequest) {
-        usersService.registerUser(usersRegisterRequest);
+    public ResponseEntity<?> registerUser(@Valid @RequestBody UsersRegisterRequest usersRegisterRequest,
+            BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().body(bindingResult.getAllErrors());
+        } else {
+            usersService.registerUser(usersRegisterRequest);
+            return ResponseEntity.ok("Success");
+        }
     }
 
 }
--- src/main/java/com/example/procon34_CYBER_WARS_backend/dto/UsersRegisterRequest.java
+++ src/main/java/com/example/procon34_CYBER_WARS_backend/dto/UsersRegisterRequest.java
@@ -2,12 +2,19 @@ package com.example.procon34_CYBER_WARS_backend.dto;
 
 import java.io.Serializable;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UsersRegisterRequest implements Serializable {
 
+    @NotBlank
+    @Size(max = 20)
     private String name;
+
+    @NotBlank
+    @Size(max = 20)
     private String password;
 
     public String getName() {
