bbfb6ad773d77b66ad4afe91e12287713bdb974b
ORIKAMIxNEP <ta.orikami@gmail.com>
2023-05-23 08:58:39 +0900

    ファイル名の変更

--- src/main/java/com/example/procon34_CYBER_WARS_backend/config/Config.java
+++ src/main/java/com/example/procon34_CYBER_WARS_backend/config/Config.java
@@ -6,10 +6,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class WebSecurityConfig {
+public class Config {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain configureHttp(HttpSecurity http) throws Exception {
         http.csrf().disable();
         return http.build();
     }
