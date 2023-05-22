4ab01289990f732bc80b73ecd159594c11b5df3a
ORIKAMIxNEP <ta.orikami@gmail.com>
2023-05-23 04:12:41 +0900

    UsersMapper.javaのテーブル操作アノテーションと関数の戻り値型の変更

--- src/main/java/com/example/procon34_CYBER_WARS_backend/repository/UsersMapper.java
+++ src/main/java/com/example/procon34_CYBER_WARS_backend/repository/UsersMapper.java
@@ -1,15 +1,14 @@
 package com.example.procon34_CYBER_WARS_backend.repository;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 import com.example.procon34_CYBER_WARS_backend.dto.UsersRegisterRequest;
-import com.example.procon34_CYBER_WARS_backend.entity.Users;
 
 @Mapper
 public interface UsersMapper {
 
-    @Select("INSERT INTO users(name, password) VALUES(#{name}, #{password})")
-    Users register(UsersRegisterRequest usersRegisterRequest);
+    @Insert("INSERT INTO users(name, password) VALUES(#{name}, #{password})")
+    void register(UsersRegisterRequest usersRegisterRequest);
 
 }
