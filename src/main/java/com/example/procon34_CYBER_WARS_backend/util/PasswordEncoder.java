8965c325f6bfbfdeadad2b330d1c359c22384d21
ORIKAMIxNEP <ta.orikami@gmail.com>
2023-05-23 08:46:56 +0900

    test

--- src/main/java/com/example/procon34_CYBER_WARS_backend/util/PasswordEncoder.java
+++ src/main/java/com/example/procon34_CYBER_WARS_backend/util/PasswordEncoder.java
@@ -1,14 +1,22 @@
 package com.example.procon34_CYBER_WARS_backend.util;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class PasswordEncoder {
 
     public String encodePassword(String password) {
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        return passwordEncoder.encode(password);
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA3-256");
+            byte[] result = messageDigest.digest(password.getBytes());
+            return String.format("%040x", new BigInteger(1, result));
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
