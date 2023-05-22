9a5e44d321c47500235ce05c4a68065bd0e10b1f
ORIKAMIxNEP <ta.orikami@gmail.com>
2023-05-23 08:52:40 +0900

    test

--- src/main/java/com/example/procon34_CYBER_WARS_backend/service/UsersService.java
+++ src/main/java/com/example/procon34_CYBER_WARS_backend/service/UsersService.java
@@ -18,21 +18,18 @@ public class UsersService {
     private PasswordEncoder passwordEncoder;
 
     public void register(UsersCredentialsRequest usersCredentialsRequest) {
-        String hashedPassword = passwordEncoder
-                .encodePassword(usersCredentialsRequest.getName() + usersCredentialsRequest.getPassword());
+        String hashedPassword = passwordEncoder.encodePassword(usersCredentialsRequest.getPassword());
         usersCredentialsRequest.setPassword(hashedPassword);
         usersMapper.register(usersCredentialsRequest);
     }
 
     public void login(UsersCredentialsRequest usersCredentialsRequest) {
-        String hashedPassword = passwordEncoder
-                .encodePassword(usersCredentialsRequest.getName() + usersCredentialsRequest.getPassword());
+        String hashedPassword = passwordEncoder.encodePassword(usersCredentialsRequest.getPassword());
         usersCredentialsRequest.setPassword(hashedPassword);
         Users users = usersMapper.login(usersCredentialsRequest);
         if (users != null) {
             System.out.println("login");
-        } else {
-            System.out.println("failed");
+            // セッション追加
         }
     }
 
