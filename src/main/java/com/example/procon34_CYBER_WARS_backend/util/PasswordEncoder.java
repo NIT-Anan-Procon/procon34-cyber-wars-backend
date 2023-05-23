12b21a23068e593d147b354f6c2c681ccde8b32e
ORIKAMIxNEP <ta.orikami@gmail.com>
2023-05-24 01:08:26 +0900

    test

--- src/main/java/com/example/procon34_CYBER_WARS_backend/util/PasswordEncoder.java
+++ src/main/java/com/example/procon34_CYBER_WARS_backend/util/PasswordEncoder.java
@@ -1,18 +1,20 @@
 package com.example.procon34_CYBER_WARS_backend.util;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PasswordEncoder {
 
+    @Autowired
+    BCryptPasswordEncoder passwordEncoder;
+
     public String encodePassword(String password) {
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         return passwordEncoder.encode(password);
     }
 
     public boolean checkPassword(String password, String hashedPassword) {
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         return passwordEncoder.matches(password, hashedPassword);
     }
 
