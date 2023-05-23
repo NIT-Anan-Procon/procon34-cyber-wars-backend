40e0c92d35045c48492fe2f53ca1dd2bfdf963c0
ORIKAMIxNEP <ta.orikami@gmail.com>
2023-05-23 22:52:28 +0900

    UsersCredentialsRequest.javaに変更

--- src/main/java/com/example/procon34_CYBER_WARS_backend/repository/UsersMapper.java
+++ src/main/java/com/example/procon34_CYBER_WARS_backend/repository/UsersMapper.java
@@ -3,12 +3,12 @@ package com.example.procon34_CYBER_WARS_backend.repository;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
-import com.example.procon34_CYBER_WARS_backend.dto.UsersRegisterRequest;
+import com.example.procon34_CYBER_WARS_backend.dto.UsersCredentialsRequest;
 
 @Mapper
 public interface UsersMapper {
 
     @Insert("INSERT INTO users(name, password) VALUES(#{name}, #{password})")
-    void register(UsersRegisterRequest usersRegisterRequest);
+    void register(UsersCredentialsRequest usersRegisterRequest);
 
 }
