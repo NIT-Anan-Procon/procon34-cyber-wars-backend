941168b2aae6dbe5d3c68842b6c03f0199296622
ORIKAMIxNEP <ta.orikami@gmail.com>
2023-07-04 15:54:44 +0900

    test

--- src/main/java/com/example/procon34_CYBER_WARS_backend/Procon34CyberWarsBackendApplication.java
+++ src/main/java/com/example/procon34_CYBER_WARS_backend/Procon34CyberWarsBackendApplication.java
@@ -2,10 +2,8 @@ package com.example.procon34_CYBER_WARS_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
-@EnableRedisHttpSession
 public class Procon34CyberWarsBackendApplication {
 
 	public static void main(String[] args) {
