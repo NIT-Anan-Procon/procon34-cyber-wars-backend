a0d6deea013a0509355f85792c2d548f72672af5
ORIKAMIxNEP <ta.orikami@gmail.com>
2023-08-24 07:36:55 +0900

    test

--- src/main/java/com/example/procon34_CYBER_WARS_backend/config/Config.java
+++ src/main/java/com/example/procon34_CYBER_WARS_backend/config/Config.java
@@ -3,6 +3,7 @@ package com.example.procon34_CYBER_WARS_backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,9 +15,9 @@ public class Config {
         return httpSecurity.build();
     }
 
-    // @Bean
-    // public BCryptPasswordEncoder bCryptPasswordEncoder() {
-    // return new BCryptPasswordEncoder();
-    // }
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
