92e0e539c57eb706026aa8d01a8ff294cd656b2f
ORIKAMIxNEP <ta.orikami@gmail.com>
2023-08-24 13:00:24 +0900

    test

--- src/main/java/com/example/procon34_CYBER_WARS_backend/dto/Rooms/CreateRoomRequest.java
+++ src/main/java/com/example/procon34_CYBER_WARS_backend/dto/Rooms/CreateRoomRequest.java
@@ -1,12 +1,12 @@
 package com.example.procon34_CYBER_WARS_backend.dto.rooms;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CreateRoomRequest {
 
-    @NotBlank
+    @NotNull
     private boolean difficult;
 
 }
