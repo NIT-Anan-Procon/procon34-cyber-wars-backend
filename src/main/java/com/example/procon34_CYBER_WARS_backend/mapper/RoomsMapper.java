4758f4193a9747daf8fa3aae4d73edda8654c3f0
ORIKAMIxNEP <ta.orikami@gmail.com>
2023-08-24 13:15:13 +0900

    test

--- src/main/java/com/example/procon34_CYBER_WARS_backend/mapper/RoomsMapper.java
+++ src/main/java/com/example/procon34_CYBER_WARS_backend/mapper/RoomsMapper.java
@@ -2,7 +2,6 @@ package com.example.procon34_CYBER_WARS_backend.mapper;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
 import com.example.procon34_CYBER_WARS_backend.entity.Allocations;
 import com.example.procon34_CYBER_WARS_backend.entity.Rooms;
@@ -12,8 +11,8 @@ import com.example.procon34_CYBER_WARS_backend.entity.Vulnerabilities;
 public interface RoomsMapper {
 
     // ルーム作成
-    @Insert("INSERT INTO rooms(invite_id, challenge_id) SELECT #{rooms.invite_id}, challenge_id FROM vulnerabilities WHERE difficult = #{vulnerabilities.difficult} ORDER BY RAND() LIMIT 1")
-    void createRoom(@Param("room") final Rooms room, @Param("vulnerability") final Vulnerabilities vulnerability);
+    @Insert("INSERT INTO rooms(invite_id, challenge_id) SELECT #{room.invite_id}, challenge_id FROM vulnerability WHERE difficult = #{vulnerabilities.difficult} ORDER BY RAND() LIMIT 1")
+    void createRoom(final Rooms room, final Vulnerabilities vulnerability);
 
     // ルーム割り当て
     @Insert("INSERT INTO allocations(room_id, user_id) SELECT MAX(room_id), #{user_id} FROM rooms")
