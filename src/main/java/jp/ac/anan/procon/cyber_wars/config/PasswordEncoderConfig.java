24f7a6a87326affeaaf9ab15d68a7ce0c3069e08
ORIKAMIxNEP <taiki.orikami@gmail.com>
2023-10-10 14:17:14 +0900

    クラス名の変更

--- src/main/java/jp/ac/anan/procon/cyber_wars/config/PasswordEncoderConfig.java
+++ src/main/java/jp/ac/anan/procon/cyber_wars/config/PasswordEncoderConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-public class Config {
+public class PasswordEncoderConfig {
   @Bean
   public BCryptPasswordEncoder bcryptPasswordEncoder() {
     return new BCryptPasswordEncoder();
